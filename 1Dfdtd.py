d762d58d2b5e9bb292306d9b3e8c6a21a269b7ae
Yifei Simon Shao <shaoyifei96@gmail.com>
2017-04-10 15:14:08 -0400

    Vpython Jupitar works for 1d

--- 1Dfdtd.py
+++ 1Dfdtd.py
@@ -9,15 +9,15 @@ from numpy import ones,zeros
 from pylab import show,figure,xlim,ylim,axes
 from matplotlib import animation
 
-m=1
+m=1.0
 cm=1e-2*m
 mm=1e-3*m
 inch=2.54*cm
-ft=12*inch
-s=1
-hz=1/s
+ft=12.0*inch
+s=1.0
+hz=1.0/s
 
-c0=299792458*m/s
+c0=299792458.0*m/s
 e0=8.854187817e-12*1/m
 u0=1.2566370614e-6*1/m
 
@@ -35,15 +35,18 @@ mEy=(c0*dt)/ER
 mHx=(c0*dt)/UR
 
 Ey=zeros(Nz)
+Ey[40]=40
 Hx=zeros(Nz)
+Hx[40]=14
 
 for t in range(step):
     for nz in range(1,Nz-1):
+    	print("H",nz)
         Hx[nz-1]=Hx[nz-1]+mHx[nz-1]*(Ey[nz]-Ey[nz-1])/dz
     Hx[Nz-1]=Hx[Nz-1]+mHx[Nz-1]*(0-Ey[Nz-1])/dz
       
     Ey[0]=Ey[0]+mEy[0]*(Hx[0]-0)/dz
     for nz in range(2,Nz):
         Ey[nz-1]=Ey[nz-1]+mEy[nz-1]*(Hx[nz-1]-Hx[nz-1])/dz
-          
+        #print(Ey[39],Ey[40],Ey[41])          
 
