10de40a5661d9f94343351f227939ed59af9d8fc
Yifei Shao <shaoyifei@Yifeis-MacBook-Pro.local>
2017-04-17 13:37:05 -0400

    bug fix

--- Source/main.py
+++ Source/main.py
@@ -17,24 +17,27 @@ import EH.Curl as cr
 import Initial_Material.Mat_Class as mat
 import copy as cp
 import matplotlib.pyplot as plt
+import time as tm
 print("Hello World")
 dt=0.5
 dx=0.1
 dy=0.1
+Nx=4
+Ny=4
 #setup, the following code should run once
 #Set Initial Conditions
 #Set Material Property
 #
 #
 #==================TEST for material class, you can add a block of material in 2d
-Mat_map=mat.Mat(10,10,dt)
+Mat_map=mat.Mat(Nx,Ny,dt)
 Mat_map.add_mat_bond(0,2,0,2,1.2,1)#(i_i,i_f,j_i,j_f,e,mu)
 print("mat=",Mat_map.e)
 #==================
 
 #======TEST PURPOSE 
 
-Ex=np.zeros((10,10),float)
+Ex=np.zeros((Nx,Ny),float)
 r,c=np.shape(Ex)
 Ex[0:r,0:c]=0
 
@@ -43,20 +46,26 @@ Ez=cp.deepcopy(Ex)
 Hx=cp.deepcopy(Ex)
 Hy=cp.deepcopy(Ex)
 Dz=cp.deepcopy(Ex)
+#inital condition
 Ez[0,0]=1
 
 #======DO NOT USE FOR FINAL
-while(True):
+n=0
+while(n<100):
 	CEx=cr.M_Ez_Curl_Ex(Ez,dy)
 	CEy=cr.M_Ez_Curl_Ey(Ez,dx)
 	Hx=lin_func.M_Ez_Hx_update(Hx,CEx,Mat_map.M_Ez_Coef_Ex)
 	Hy=lin_func.M_Ez_Hy_update(Hy,CEy,Mat_map.M_Ez_Coef_Ey)
 	CHz=cr.M_Ez_Curl_Hz(Hx, Hy, dx, dy)
 	Dz=lin_func.M_Ez_Dz_update(Dz,CHz,Mat_map.M_Ez_Coef_Hz)
+	#add in source here
 	Ez=lin_func.M_Ez_Ez_from_Dz(Dz, Mat_map.M_Ez_Coef_Dz)
-	print(Ez)
-	#Loop
-	break
+	print("Ez=",Ez)
+
+	n=n+1
+	tm.sleep(3)
+
+
 	#Update D from H
 	#Update E from D
 	#Handle E field boundary(boundary means edge, not material)
