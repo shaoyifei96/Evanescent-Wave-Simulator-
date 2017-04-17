43c08936ea4efc354875a20d5b86f80f44b19082
Yifei Shao <shaoyifei@Yifeis-MBP.fios-router.home>
2017-04-16 23:01:39 -0400

    intialize with free space in mat

--- Source/Initial_Material/Mat_Class.py
+++ Source/Initial_Material/Mat_Class.py
@@ -1,11 +1,12 @@
 import numpy as np
 class Mat:
-
 	def __init__(self, row, col):
+		e0=8.854187817e-12
+		mu0=1.2566370614e-6
 		self.row= row
 		self.col= col
-		self.e=np.zeros((row, col),float)
-		self.mu=np.zeros((row, col),float)
+		self.e=np.full((row, col),e0)
+		self.mu=np.full((row, col),mu0)
 
 	def add_mat_bond(self,i_lower,i_upper,j_lower,j_upper,e,mu):#add a block of 2D material
 		#This is rather complicated, will consider more
@@ -13,7 +14,7 @@ class Mat:
 			for j in range (j_lower,j_upper):
 				self.e[i,j]=e
 				self.mu[i,j]=mu
-				
+
 			
 			
 
